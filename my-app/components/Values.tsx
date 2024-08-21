775b0e397622d2a69ff3b455bfa22819a144edac
Ali Ghieth <alighieth2709@gmail.com>
2024-08-21 20:32:02 +0300

    add responsivness to skills

--- my-app/components/Values.tsx
+++ my-app/components/Values.tsx
@@ -14,25 +14,22 @@ const skills: string[] = [
 const Values = async () => {
   const t = await getScopedI18n("products");
   return (
-    <section id="products" className="grid grid-cols-2">
-      <div className="skills font-bold">
-        <h4 className="mb-2">{t("skills")}</h4>
+    <section id="products" className="p-5 text-justify md:p-0 md:grid md:grid-cols-2">
+      <div className="skills">
+        <h4 className="mb-2 font-bold">{t("skills")}</h4>
         <ol className="font-thin">
           {skills.map((skill) => (
             <li key={skill}>{skill}</li>
           ))}
         </ol>
       </div>
-      <div className="passion font-bold">
-        <h4 className="mb-2">{t("passion")}</h4>
-        <p className="font-thin text-justify">{t("passionInfo")}</p>
+
+      <div className="passion mt-5">
+        <h4 className="mb-2  font-bold">{t("passion")}</h4>
+        <p className="font-thin">{t("passionInfo")}</p>
       </div>
     </section>
   );
-};
-
-const JobComponent = (props: { name: string }) => {
-  return <div></div>;
 };
 
 export default Values;
